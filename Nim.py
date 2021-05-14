bdd1a5dc94705b03b4a299a1f5f6a6a3cb2f8f8b
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2021-05-14 22:38:13 +1000

    Add files via upload

--- Nim.py
+++ Nim.py
@@ -1,6 +1,6 @@
 import random
 
-# Initial values of Aplha and Beta
+# Initial values of Beta, Alpha is set the the number of stones in all the piles
 MIN = 0
 
 
@@ -11,223 +11,132 @@ def minimax(depth, nodeIndex, maximizingPlayer, values, alpha, beta):
     # leaf node is reached
     if depth == 3:
         return values[nodeIndex]
-
+    # checks to see if its the max player's turn
     if maximizingPlayer:
 
         best = MIN
 
         # Recur for left and right children
         for i in range(0, 2):
-
+            # uses recursion to traverse to depth 3
             val = minimax(depth + 1, nodeIndex, False, values, alpha, beta)
+            # updates the best value by comparing to to the aplha
             best = max(best, val)
             alpha = max(alpha, best)
 
             # Alpha Beta Pruning
             if beta <= alpha:
                 break
-
+        # the function ends and returns the chosen value
         return best
-
+    # if
     else:
+        # begins finding the value for the min player
         best = MAX
 
         # Recur for left and
         # right children
         for i in range(0, 2):
-
+            # uses recursion to traverse to depth 3
             val = minimax(depth + 1, nodeIndex, True, values, alpha, beta)
+            # updates the best value by comparing to to the beta
             best = min(best, val)
             beta = min(beta, best)
 
             # Alpha Beta Pruning
             if beta <= alpha:
                 break
-
+        # the function ends and returns the chosen value
         return best
 
-
+# sets up the game by creating the piles
 def game_setup():
+    # piles are between 2-5
     n = random.randrange(2, 6)
-    print(n)
+    # pieces are calculated by n which is the number of piles
     pieces = 2 * n + 1
     Pile = {}
     for x in range(1, n + 1):
         Pile[x] = 1
 
+    # distributes the pieces as evenly as possible
     while sum(Pile.values()) < pieces:
         for x in range(1, n + 1):
             if sum(Pile.values()) < pieces:
                 Pile[x] += 1
-
+    # returns a dictionary of piles filled with pieces
     return Pile
-    print(Pile)
+
 
 
 human = int(0)  # Initialize the number of times humans win the game
-computer = int(0)
+computer = int(0) # Initialize the number of times computer/AI win the game
 play_str = input("Would you like to play? (0=no, 1=yes) ")
 
 while int(play_str) != 0:  # use while to determine if the game starts
-    Pile = game_setup()
 
+    Pile = game_setup()
+    # determines whether the game loop continues
+    game = True
+    # displays a list of the keys and value which are the piles and pieces
     print("Start _--_")
     for x in Pile.keys():
         print("Pile ", x, ":", Pile[x])
 
-    while (Pile[1] > 0) or (Pile[2] > 0):  # use while to determine if the pile is empty.
+    while game:  # use while to determine if the pile is empty.
+
         pile = int(input("Choose a pile: "))
-
-        while Pile[pile] == 0:
-            # If one of the piles is empty. No more stones can be removed from this pile.
-
-            print("Pile must be 1 or 2 and non-empty. Please try again.")
+        while pile < 1 or pile > len(Pile):
+            print("Sorry, the pile must exist.")
             pile = int(input("Choose a pile: "))
 
-        else:
+        stones = int(input("Choose stones to remove from pile: "))
+        while stones < 1 or stones > max(Pile.values()):
+            print("Sorry, you can only remove stones that are in a pile.")
+            stones = int(input("Choose stones to remove from pile: "))
+        # player removes stone from a pile
+        Pile[pile] = Pile[pile] - stones
+        print("Player -> ", end='')
+        print("Remove", stones, "stones from pile", pile)
+        for x in Pile.keys():
+            print("Pile ", x, ":", Pile[x])
 
-            while (pile < 0) or (pile > len(Pile)):
-                # If the number of the pile is not legal. Can't continue.
 
-                print("Pile must be 1 or 2 and non-empty. Please try again.")
-                pile = int(input("Choose a pile: "))
+        if all(value == 0 for value in Pile.values()):
+            human = human + 1  # Humans win the game plus one.
+            print("\nPlayer wins!")
+            print("Score -> human:", human, "; computer:", computer)
+            # determines whether1 the game loop continues
+            game = False
+            break
 
-            else:
-                stones = int(input("Choose stones to remove from pile: "))
+        # Alpha-Beta pruning algorithm begins
+        # The MAX is the total number of stones remaining
+        MAX = sum(Pile.values())
+        values = list(Pile.values())
+        # removes 0 from the list of values, you must take aleast 1 stone
+        values[:] = (x for x in values if x != 0)
+        # algorithm returns the best number of stones to remove
+        amount = minimax(0, 0, True, values, MIN, MAX)
+        for x in Pile:
+            if Pile[x] == amount:
+                break
 
-                while (stones > 3) or (stones < 1):
-                    # If the number of the stones is not beween 1 and 3. Can't continue.
+        # AI removes stone from a pile
+        Pile[x] = Pile[x] - amount
+        print("Computer -> ", end='')
+        print("Remove", amount, "stones from pile", x)
+        for x in Pile.keys():
+            print("Pile ", x, ":", Pile[x])
 
-                    print("Invalid number of stones. Please try again.")
-                    stones = int(input("Choose stones to remove from pile: "))
+        if all(value == 0 for value in Pile.values()):
+            # The stones was taken empty by computer.
 
-                else:
+            computer = computer + 1  # Computer win the game plus one.
+            print("\nComputer wins!")
+            print("Score -> human:", human, "; computer:", computer)
+            # determines whether the game loop continues
+            game = False
 
-                    print("Player -> ", end='')
-                    print("Remove", stones, "stones from pile", pile)
-
-                    # Next, use the ”if“ to divide the two cases.
-
-                    # The first is when humans choose to use the Pile[1.
-                    # Then the computer judges the Pile[2 first.
-                    # If the Pile[2 is empty.
-                    # Judging the Pile[1.
-                    # Once who is the last stone.
-                    # Who wins.
-
-                    # The second is when humans choose to use the Pile[2.
-                    # Then the computer judges the Pile[1 first.
-                    # If the Pile[1 is empty.
-                    # Judging the Pile[2.
-                    # Once who is the last stone.
-                    # Who wins.
-
-                    if pile > -1:
-                        # If it is Pile[1 and there are enough stones in the Pile[1.
-
-                        Pile[pile] = Pile[pile] - stones
-                        for x in Pile.keys():
-                            print("Pile ", x, ":", Pile[x])
-
-                        if all(value == 0 for value in Pile.values()):
-                            # If the piles are taken empty.
-
-                            human = human + 1  # Humans win the game plus one.
-                            print("\nPlayer wins!")
-                            print("Score -> human:", human, "; computer:", computer)
-
-                        else:
-                            # The stones was not taken by humans.
-
-                            if (Pile[2] > 0):
-                                # If the Pile[2 is not taken empty.
-                                MAX = sum(Pile.values())
-                                values = list(Pile.values())
-                                list(filter(lambda num: num != 0, values))
-                                amount = minimax(0, 0, True, values, MIN, MAX)
-                                for x in Pile:
-                                    if Pile[x] == amount:
-                                        break
-
-                                Pile[x] = Pile[x] - amount
-                                print("Computer -> ", end='')
-                                print("Remove", amount, "stones from pile", x)
-
-                            else:
-                                # If the Pile[2 is taken empty.
-                                MAX = sum(Pile.values())
-                                values = list(Pile.values())
-                                list(filter(lambda num: num != 0, values))
-                                amount = minimax(0, 0, True, values, MIN, MAX)
-                                for x in Pile:
-                                    if Pile[x] == amount:
-                                        break
-
-                                Pile[x] = Pile[x] - amount
-                                print("Computer -> ", end='')
-                                print("Remove", amount, "stones from pile", x)
-
-                            if all(value == 0 for value in Pile.values()):
-                                # The stones was taken empty by computer.
-
-                                computer = computer + 1  # Computer win the game plus one.
-                                print("\nComputer wins!")
-                                print("Score -> human:", human, "; computer:", computer)
-
-                    if (pile == 2) and (Pile[2] >= stones):
-                        # If it is Pile[2 and there are enough stones in the Pile[2.
-
-                        Pile[2] = Pile[2] - stones
-                        for x in Pile.keys():
-                            print("Pile ", x, ":", Pile[x])
-
-                        if all(value == 0 for value in Pile.values()):
-                            # If the piles are taken empty.
-
-                            human = human + 1  # Humans win the game plus one.
-                            print("\nPlayer wins!")
-                            print("Score -> human:", human, "; computer:", computer)
-
-                        else:
-                            # The stones was not taken by humans.
-
-                            if (Pile[1] > 0):
-                                # If the Pile[1 is not taken empty.
-                                MAX = sum(Pile.values())
-                                values = list(Pile.values())
-                                list(filter(lambda num: num != 0, values))
-                                amount = minimax(0, 0, True, values, MIN, MAX)
-                                for x in Pile:
-                                    if Pile[x] == amount:
-                                        break
-
-                                Pile[x] = Pile[x] - amount
-                                print("Computer -> ", end='')
-                                print("Remove", amount, "stones from pile", x)
-
-                            else:
-                                # If the Pile[1 is taken empty.
-                                MAX = sum(Pile.values())
-                                values = list(Pile.values())
-                                list(filter(lambda num: num != 0, values))
-                                amount = minimax(0, 0, True, values, MIN, MAX)
-                                for x in Pile:
-                                    if Pile[x] == amount:
-                                        break
-
-                                Pile[x] = Pile[x] - amount
-                                print("Computer -> ", end='')
-                                print("Remove", amount, "stones from pile", x)
-                                for x in Pile.keys():
-                                    print("Pile ", x, ":", Pile[x])
-
-                            if all(value == 0 for value in Pile.values()):
-                                # The stones was taken empty by computer.
-
-                                computer = computer + 1  # Computer win the game plus one.
-                                print("\nComputer wins!")
-                                print("Score -> human:", human, "; computer:", computer)
-
-    else:
-        play_str = input("\nWould you like to play again? (0=no, 1=yes) ")
+    play_str = input("\nWould you like to play again? (0=no, 1=yes) ")
         # Judge whether to come again
